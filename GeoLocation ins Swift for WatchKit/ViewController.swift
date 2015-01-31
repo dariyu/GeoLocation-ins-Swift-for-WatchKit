203359041c192e410d306f803fa293568f7abd23
patut <patutinskiy-feod@mail.ru>
2015-01-31 13:56:51 +0300

    Initial Commit

---
+++ GeoLocation ins Swift for WatchKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoLocation ins Swift for WatchKit
+//
+//  Created by Патутинский Федор on 31.01.15.
+//  Copyright (c) 2015 BitFountain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GeoLocation ins Swift for WatchKitTests/GeoLocation_ins_Swift_for_WatchKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  GeoLocation_ins_Swift_for_WatchKitTests.swift
+//  GeoLocation ins Swift for WatchKitTests
+//
+//  Created by Патутинский Федор on 31.01.15.
+//  Copyright (c) 2015 BitFountain. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GeoLocation_ins_Swift_for_WatchKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
